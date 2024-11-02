16c555465d4d7ed1010b5c80456279da5b7256f4
Motjiang <motjiangbrilliant@gmail.com>
2024-11-02 13:37:13 +0200

    DbContext

--- RelationShips/Data/ApplicationDbContext.cs
+++ RelationShips/Data/ApplicationDbContext.cs
@@ -1,6 +1,19 @@
-﻿namespace RelationShips.Data
+﻿using Microsoft.EntityFrameworkCore;
+using RelationShips.Models.Domains;
+
+namespace RelationShips.Data
 {
-    public class ApplicationDbContext 
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        // Add the following DbSet : Images, Posts, Blogs
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+
     }
 }
